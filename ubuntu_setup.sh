8abeddad890302473cb1278e6079e8c2b7015297
tristan3fish <tristan3fish@hotmail.com>
2026-04-15 17:28:19 +0100

    Add Ubuntu setup script for installing packages

---
+++ ubuntu_setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install curl
+sudo apt install curl
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl -fsSL https://openclaw.ai/install.sh | bash
