cc8cb5d588cbe3511d5e7ca6963e58a2875459f6
tristan3fish <tristan3fish@hotmail.com>
2026-04-15 18:08:52 +0100

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2,5 +2,13 @@
 sudo apt-get update
 sudo apt install curl
 sudo apt-get install git-all
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> ~/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+sudo apt-get install build-essential
+brew install gcc
+
 curl -fsSL https://openclaw.ai/install.sh | bash
