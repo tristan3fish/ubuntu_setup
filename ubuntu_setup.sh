4f8b71aa720656e56abcac78909bac2656ac83e4
tristan3fish <tristan3fish@hotmail.com>
2026-04-15 17:44:52 +0100

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,4 +1,6 @@
-sudo apt-get install curl
+#// sudo apt-get install curl
+sudo apt-get update
 sudo apt install curl
+sudo apt-get install git-all
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl -fsSL https://openclaw.ai/install.sh | bash
